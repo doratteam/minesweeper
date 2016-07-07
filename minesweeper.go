55a774d6cc188fc063109355bf55ff5c2518d63d
Tai Won Chung <tchung1118@gmail.com>
2016-07-07 13:40:01 +0900

    Initial commit

---
+++ minesweeper.go
@@ -0,0 +1,12 @@
+package main
+
+/*********************************
+Auther: Tai Won Chung
+Description: This is the main driver for minesweeper game
+*********************************/
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
